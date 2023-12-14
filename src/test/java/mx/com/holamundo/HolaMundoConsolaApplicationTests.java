12f264414553fe89a594e1b30eb47eb7e7017292
ShinjiVaquero <trujillohernandezmustang@yahoo.com.mx>
2023-12-13 20:24:05 -0600

    Creación del proyecto: Hola mundo

---
+++ src/test/java/mx/com/holamundo/HolaMundoConsolaApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoConsolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
